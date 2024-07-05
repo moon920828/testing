139ef6d1bbb0baed4812c04e503c687438dc5493
moon920828 <seong.jae.moon.1992@gmail.com>
2024-07-05 12:54:10 +0000

    test

--- script.sh
+++ script.sh
@@ -48,15 +48,17 @@ echo $REQUEST_BODY
 
 GET_RESPONSE=$(curl -s -X GET -H "Authorization: Bearer $ACCESS_TOKEN" "$GET_POST_API_URL")
 
-if [[ "$GET_RESPONSE" == *"\"error\": {"* && "$GET_RESPONSE" == *"\"code\": 404"* ]]; then
-  echo "Error code is 404, Making a PUT request."
+echo $GET_RESPONSE
 
-  # Make the PUT request
-  curl -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$PATCH_API_URL"
-else
-  echo "No error code 404 found, Making a POST request."
+# if [[ "$GET_RESPONSE" == *"\"error\": {"* && "$GET_RESPONSE" == *"\"code\": 404"* ]]; then
+#   echo "Error code is 404, Making a PUT request."
 
-  # Make the POST request
-  curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$GET_POST_API_URL"
-fi
+#   # Make the PUT request
+#   curl -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$PATCH_API_URL"
+# else
+#   echo "No error code 404 found, Making a POST request."
+
+#   # Make the POST request
+#   curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$GET_POST_API_URL"
+# fi
 
