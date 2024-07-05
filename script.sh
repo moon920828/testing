527eb1fff10accf2976889dd9137e351d8f0a8b7
moon920828 <seong.jae.moon.1992@gmail.com>
2024-07-05 10:26:54 +0000

    test

---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Authenticate and obtain access token
+ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
+
+# Retrieve the secret from Secret Manager
+# SECRET=$(gcloud secrets versions access latest --secret=MY_SECRET)
+SECRET="https://www.googleapis.com/auth/pubsub"
+
+# API URL
+API_URL="https://integrations.googleapis.com/v1/projects/my-project-1553458465069/locations/us-central1/authConfigs"
+
+# Create the JSON request body
+# REQUEST_BODY=$(jq -n --arg key1 "value1" --arg key2 "$SECRET" '{key1: $key1, key2: $key2}')
+REQUEST_BODY=$(cat <<EOF
+{
+    "name": "projects/my-project-1553458465069/locations/us-central1/authConfigs/cloudbuild-test-1"
+    "displayName": "testing",
+    "visibility": "CLIENT_VISIBLE",
+    "state": "VALID",
+    "decryptedCredential": {
+        "credentialType": "SERVICE_ACCOUNT",
+        "serviceAccountCredentials": {
+            "serviceAccount": "test-91@my-project-1553458465069.iam.gserviceaccount.com",
+            "scope": "$SECRET"          
+        }
+      }
+}
+EOF
+)
+
+# Make the POST request
+curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$API_URL"
--- test.sh
+++
@@ -1,3 +0,0 @@
-echo 'running'
-
-$HOME/.integrationcli/bin/integrationcli integrations list --verbose
