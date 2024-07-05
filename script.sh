fce1bf310c35aa9bd718c87a24acbf3d1a4215ed
moon920828 <seong.jae.moon.1992@gmail.com>
2024-07-05 13:02:05 +0000

    test

--- script.sh
+++ script.sh
@@ -58,9 +58,9 @@ if [[ "$GET_RESPONSE" == *"\"error\": {"* && "$GET_RESPONSE" == *"\"code\": 404"
  # Make the POST request
   curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$GET_POST_API_URL"
 else
-  echo "No error code 404 found, Making a PUT request."
+  echo "No error code 404 found, Making a PATCH request."
 
-  # Make the PUT request
-  curl -X PUT -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$PATCH_API_URL"
+  # Make the PATCH request
+  curl -X PATCH -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$PATCH_API_URL"
 fi
 
