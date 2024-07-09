d0b86c219456552d3ed8e81d75546b40ef6e96f6
moon920828 <seong.jae.moon.1992@gmail.com>
2024-07-09 09:29:44 +0000

    test

--- cloudFunction/index.js
+++ cloudFunction/index.js
@@ -1,5 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('hey', (req, res) => {
+  const hello = 'hello';
   res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
 });
