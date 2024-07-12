e77012057a1ec6024f02b5c9c3cd71d18546d55a
moon920828 <seong.jae.moon.1992@gmail.com>
2024-07-12 14:59:25 +0000

    test

--- scripts/step2.sh
+++ scripts/step2.sh
@@ -4,3 +4,7 @@ echo '----------step2-----------'
 
 # Exit immediately if a command exits with a non-zero status
 set -e
+
+ACCESS_TOKEN=$(cat /workspace/access_token.txt)
+
+echo $ACCESS_TOKEN
--- scripts/step3.sh
+++ scripts/step3.sh
@@ -1,1 +1,10 @@
-echo 'step3'
+#!/bin/bash
+
+echo '----------step3-----------'
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+ACCESS_TOKEN=$(cat /workspace/access_token.txt)
+
+echo $ACCESS_TOKEN
