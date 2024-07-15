0224600ae0254c81d828893c2d975366c7b4eebc
moon920828 <seong.jae.moon.1992@gmail.com>
2024-07-15 10:38:36 +0000

    test

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,1 +1,16 @@
 console.log(process.env)
+
+const envFile = '/workspace/env_vars.txt';
+
+// Read environment variables from file
+const envVarsData = await fs.readFile(envFile, 'utf8');
+const envVars = parseEnvVars(envVarsData);
+
+// Access individual variables
+const {
+    base_api_url,
+    token,
+    base_resource_name
+} = envVars;
+
+console.log(envVars)
